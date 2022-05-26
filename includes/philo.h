a552ab2ada4ee542a2a6f395641a854af96e5f72
elpastorr <89456448+elpastorr@users.noreply.github.com>
2022-05-26 18:27:52 +0200

    Add files via upload

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: elpastor <elpastor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 17:45:18 by elpastor          #+#    #+#             */
-/*   Updated: 2022/05/24 17:51:28 by elpastor         ###   ########.fr       */
+/*   Updated: 2022/05/26 18:18:41 by elpastor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,24 @@
 #include <stdio.h>
 #include <sys/time.h>
 
+typedef struct s_philo
+{
+    int             n;
+    int             max_eat;
+    pthread_mutex_t *r_fork;
+    pthread_mutex_t *l_fork;
+    pthread_t       thread;
+}   t_philo;
+
+
 typedef struct s_env {
-    int t_die;
-    int t_sleep;
-    int t_eat;
-    int philo;
-    int max_eat;
+    int         t_die;
+    int         t_sleep;
+    int         t_eat;
+    int         n_philo;
+    int         max_eat;
+    long int    time;
+    t_philo     *philos;
 }   t_env;
 
 #endif
